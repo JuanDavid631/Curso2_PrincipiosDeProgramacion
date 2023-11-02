7cb2dda9febbc01404068a368db41a9df573b97b
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 00:42:08 -0500

    Clase 1

--- src/main/java/udemy/poo/codigo/Persona.java
+++ src/main/java/udemy/poo/codigo/Persona.java
@@ -17,5 +17,35 @@ public class Persona {
         this.cedula = cedula;
     }
 
-    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
 }
