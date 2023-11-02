c7e091ab066baed6287d9d7954830fa15dddc91a
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 00:54:40 -0500

    Clase 2

--- src/main/java/udemy/poo/inicio/Inicio.java
+++ src/main/java/udemy/poo/inicio/Inicio.java
@@ -1,5 +1,15 @@
 package udemy.poo.inicio;
 
+import udemy.poo.codigo.Persona;
+
+import javax.swing.*;
+
 public class Inicio {
-    
+  public static void main(String[] args) {
+    Persona persona = new Persona("Samuel", "Mendez", "Perez", "1014255632");
+    Persona persona1 = new Persona();
+    JOptionPane.showMessageDialog(null, persona.getNombre());
+    JOptionPane.showMessageDialog(null, persona1.getNombre());
+
+  }
 }
