c2f5d2db2e6bc952b462d08e9fb177202c6ef594
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 00:34:30 -0500

    Clase 1

--- src/main/java/udemy/poo/codigo/Persona.java
+++ src/main/java/udemy/poo/codigo/Persona.java
@@ -1,5 +1,8 @@
 package udemy.poo.codigo;
-
+/*
+    Programa que me de personas (nombre, apellidos, cedula)
+ */
 public class Persona {
-    
+    private String nombre, apellidoPaterno, apellidoMaterno;
+    private  String cedula;
 }
