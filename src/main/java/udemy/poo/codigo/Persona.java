9bd09d650c980eb3ec1633d925e763a7a511400f
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 00:38:08 -0500

    Clase 1

--- src/main/java/udemy/poo/codigo/Persona.java
+++ src/main/java/udemy/poo/codigo/Persona.java
@@ -5,4 +5,17 @@ package udemy.poo.codigo;
 public class Persona {
     private String nombre, apellidoPaterno, apellidoMaterno;
     private  String cedula;
+
+    public Persona(){
+
+    }
+
+    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno, String cedula) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.cedula = cedula;
+    }
+
+    
 }
